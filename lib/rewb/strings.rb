bf680e74f847a4fbf01df220d91c04ea6c381cf6
Daniel Rødskog <danielmero@gmail.com>
2015-10-25 18:50:16 +0100

    Refined String#split to also take array

--- lib/rewb/strings.rb
+++ lib/rewb/strings.rb
@@ -1,3 +1,21 @@
+module StringRefinement
+
+  def split(pattern=($;||" "), limit=-1)
+    if pattern.is_a? String or pattern.is_a? Regexp
+      super
+    elsif pattern.is_a? Enumerable
+      s = self
+      pattern.map { |w| _, s = s.split(w, 2); _ } << s
+    else
+      super(pattern.to_s, limit)
+    end
+  end
+
+end
+
+
 class String
+  prepend StringRefinement
+
   alias :/ :split
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -1,16 +1,36 @@
 require 'rewb'
 
 describe String do
-  describe "/" do
+  describe :split do
+    it "defaults to splitting on $; or spaces" do
+      expect("witness me".split).to eql %w(witness me)
+
+      $;=/(?<=\w)[^e]+/
+      expect("witness me".split).to eql %w(w e e)
+    end
+
+    it "works like before with strings and regexps" do
+      expect("hey_you".split "_").to eql %w(hey you)
+      expect("hey@you".split(/[^a-z]/)).to eql %w(hey you)
+    end
+
+    it "splits on first occurence of each subsequent element in an array" do
+      a = ["Aa", "B", "C"]
+      
+      expect("aAaAabBBbCCc".split a).to eql %w(a Aab Bb Cc)
+    end
+  end
+
+  describe :/ do
     it "acts like split" do
       result = "aaaxbbbxccc" / ?x
 
       expect(result).to eql %w(aaa bbb ccc)
     end
 
-    it "negates `:*` on string arrays" do
+    it "negates :* on string arrays" do
       a = %w(one two three)
-      s = ","
+      s = "---"
 
       expect(a*s/s).to eql a
     end
