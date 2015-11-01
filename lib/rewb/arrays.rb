1e2e564f0404c070eefcb59ba7a451c2e2339510
Daniel Rødskog <danielmero@gmail.com>
2015-11-01 22:45:35 +0100

    Added Array#deep_map

--- lib/rewb/arrays.rb
+++ lib/rewb/arrays.rb
@@ -2,4 +2,15 @@ class Array
   def ~@
     reverse
   end
+
+  def deep_map
+    self.map do |e|
+      if e.is_a? Array
+        e.deep_map { |d| yield d }
+      else
+        yield e
+      end
+    end
+  end
+
 end
