9c1670b941913ac74d5d87ecb19748443f1dd68d
Daniel Rødskog <danielmero@gmail.com>
2015-11-08 21:59:59 +0100

    Added Enumerable#group

--- lib/rewb/enumerables.rb
+++ lib/rewb/enumerables.rb
@@ -2,4 +2,16 @@ module Enumerable
   def % i
     select.with_index { |e,j| j%i == 0 }
   end
+
+  def group
+    count = Hash.new(0)
+
+    if block_given?
+      each { |e| count[yield e]+= 1 }
+    else
+      each { |e| count[e]+= 1 }
+    end
+
+    count
+  end
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -9,4 +9,17 @@ describe Enumerable do
     end
   end
 
+  describe :group do
+    let(:elements) { [1, 2, 3, 4, 4, 4, 3, 2, 2, 2, 1] }
+
+    it "groups and counts the elements of the array" do
+      expect(elements.group).to eql({1=>2, 2=>4, 3=>2, 4=>3})
+    end
+
+    it "groups by result of block if given" do
+      expect(elements.group { |e| e % 2 }).to eql({0=>7, 1=>4})
+    end
+
+  end
+
 end
