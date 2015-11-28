7f10e3d23c939a8e3c3f6f51e5b412e139684a0c
Daniel Rødskog <danielmero@gmail.com>
2015-11-28 17:42:02 +0100

    Added a usage example of enumarable integers to the specs

--- spec/integers_spec.rb
+++ spec/integers_spec.rb
@@ -2,6 +2,9 @@ require 'rewb'
 
 describe Integer do
   it "is enumerable!" do
+    a=*4
+    expect(a).to eq [0, 1, 2, 3]
+
     expect(5.map { |i| i*10 }).to eql [0, 10, 20, 30, 40]
 
     expect(3.cycle.take(10)).to eql [0, 1, 2, 0, 1, 2, 0, 1, 2, 0]
