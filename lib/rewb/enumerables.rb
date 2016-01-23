f2c9b38873c3c548b66447f5d5b3ced52bb83a28
Daniel Rødskog <danielmero@gmail.com>
2016-01-23 14:47:44 +0100

    Grouped custom Enumerable methods under Rewb module

--- lib/rewb/enumerables.rb
+++ lib/rewb/enumerables.rb
@@ -1,17 +1,25 @@
-module Enumerable
-  def % i
-    select.with_index { |e,j| j%i == 0 }
-  end
-
-  def group
-    count = Hash.new(0)
-
-    if block_given?
-      each { |e| count[yield e]+= 1 }
-    else
-      each { |e| count[e]+= 1 }
+module Rewb
+  module Enumerable
+    def %(i)
+      select.with_index { |e,j| j%i == 0 }
     end
 
-    count
+    def group
+      count = Hash.new(0)
+
+      if block_given?
+        each { |e| count[yield e]+= 1 }
+      else
+        each { |e| count[e]+= 1 }
+      end
+
+      count
+    end
   end
 end
+
+classes = Module.constants.map { |m| Module.const_get(m) }.grep(Class)
+enumerables = classes.select { |c| c.included_modules.include?(Enumerable) }
+
+enumerables.each { |e| e.include Rewb::Enumerable }
+
